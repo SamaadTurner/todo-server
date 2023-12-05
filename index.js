9312ccb0f15b4714d0d4991a360fbdc43bb0750e
SamaadTurner <samaad.turner@gmail.com>
2023-12-04 16:19:47 -0800

    added two users

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 
 require('dotenv').config();
-const { sequelize } = require('./lib/models');
+const {seedDatabase} = require('./lib/Helper/seedDatabase.js'); 
+const { sequelize, UserModel } = require('./lib/models');
 const { start } = require('./lib/server.js');
 
 const PORT = process.env.PORT || 3001;
 
 sequelize.sync()
   .then(() => {
+    seedDatabase(UserModel);
     start(PORT);
   });
